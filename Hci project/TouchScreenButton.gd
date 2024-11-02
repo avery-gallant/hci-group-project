4207515da8a172a995b7a9e067ffc797462fa5ce
avery-gallant <64525452+avery-gallant@users.noreply.github.com>
2024-11-02 18:33:14 -0300

    integraated soud buttons

--- Hci project/TouchScreenButton.gd
+++ Hci project/TouchScreenButton.gd
@@ -1,8 +1,7 @@
 extends TouchScreenButton
 var music
 var audio_loader
-var image = Image.load_from_file("res://icon.svg")
-var texture = ImageTexture.create_from_image(image)
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	
