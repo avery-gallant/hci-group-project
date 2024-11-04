7dc721f33128c2947eabad53e9a89c60020224f6
avery-gallant <64525452+avery-gallant@users.noreply.github.com>
2024-11-03 20:36:06 -0400

    hdwahda

--- Hci project/scripts/metronome.gd
+++ Hci project/scripts/metronome.gd
@@ -2,13 +2,23 @@ extends Node2D
 
 @export var bpm : float
 @export var timeSig : int
+@export var shapeOffsetX : int
+@export var shapeOffsetY : int
+@export var shapeScale : int
 var beat : int
-
+var shapePts : PackedVector2Array = []
 func _ready():
 	beat = -1
 	bpm = $bpmSlider.value
 	$Timer.wait_time = 60/bpm
 	$bpmSlider/Label.text = str(bpm)
+	
+	for i in timeSig:
+		var t = i*(2*PI/timeSig)
+		shapePts.append(Vector2(shapeScale*sin(t)+shapeOffsetX, shapeScale*cos(t)+shapeOffsetY))
+
+func _process(delta):
+	pass
 
 func _on_timer_timeout():
 	beat = (1 + beat)%timeSig
@@ -27,3 +37,9 @@ func _on_toggle_metronome_toggled(toggled_on):
 		beat = 0
 	else:
 		$Timer.stop()
+		
+func _draw() -> void:
+	var godot_blue : Color = Color("478cbf")
+	# We pass the PackedVector2Array to draw the shape.
+	draw_polygon(shapePts, [ godot_blue ])
+		
